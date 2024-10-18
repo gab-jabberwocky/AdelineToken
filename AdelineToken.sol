e97427a4d43c69ffa41e02a2cb413af2952b7afc
UwU <82875748+gab-jabberwocky@users.noreply.github.com>
2024-10-18 20:24:43 +0800

    Update AdelineToken.sol

--- AdelineToken.sol
+++ AdelineToken.sol
@@ -1,1 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
+contract AdelineToken is ERC20, ERC20Permit { 
+    constructor() ERC20("AdelineToken", "ADT") ERC20Permit("AdelineToken") {
+        _mint(msg.sender, 1000*10**decimals());
+    }
+}
