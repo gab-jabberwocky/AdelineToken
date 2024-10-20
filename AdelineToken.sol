430925f96e9a1436e140734d728f4ca475ce1225
UwU <82875748+gab-jabberwocky@users.noreply.github.com>
2024-10-20 14:52:56 +0800

    Update AdelineToken.sol

--- AdelineToken.sol
+++ AdelineToken.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
-contract AdelineToken is ERC20, ERC20Permit { 
-    constructor() ERC20("AdelineToken", "ADT") ERC20Permit("AdelineToken") {
-        _mint(msg.sender, 1000*10**decimals());
+contract AdelineToken is ERC20, ERC20Permit {  //修改AdelineToken成为你Token的名字
+    constructor() ERC20("AdelineToken", "ADT") ERC20Permit("AdelineToken") { //修改AdelineToken成为你Token的名字，修改ADT成为你Token的symbol - 通常是你token的名字的3个英文字母）
+        _mint(msg.sender, 1000*10**decimals()); //将1000该厂你token的发行量
     }
 }
